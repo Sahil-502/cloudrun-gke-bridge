2c938a50f3af65358b107652b538894d38eb98a3
Sahil <sahilhr24@gmail.com>
2025-10-05 23:29:00 +0530

    v5708

--- demo.sh
+++ demo.sh
@@ -3,19 +3,20 @@
 
  Directory of D:\Git-502
 
-10-09-2025  12:28 AM    <DIR>          .
-10-09-2025  12:28 AM    <DIR>          ..
-10-09-2025  12:29 AM    <DIR>          cicd-private-gke
-10-09-2025  12:28 AM               360 cicd-private-gke.bat
+05-10-2025  05:13 PM    <DIR>          .
+05-10-2025  05:13 PM    <DIR>          ..
+05-10-2025  11:27 PM    <DIR>          cicd-private-gke
+05-10-2025  11:27 PM               384 cicd-private-gke.bat
 10-09-2025  12:32 AM    <DIR>          cloudrun-gke-bridge
-10-09-2025  12:30 AM               366 cloudrun-gke-bridge.bat
-10-09-2025  12:32 AM    <DIR>          Infra-CLI-Guide
-10-09-2025  12:31 AM               358 Infra-CLI-Guide.bat
+05-10-2025  11:27 PM               390 cloudrun-gke-bridge.bat
+04-10-2025  02:17 PM    <DIR>          Infra-CLI-Guide
+05-10-2025  11:28 PM               382 Infra-CLI-Guide.bat
 09-09-2025  11:34 PM    <DIR>          Pipeline-For_GCP_VM
-10-09-2025  12:31 AM               366 Pipeline-For_GCP_VM.bat
+05-10-2025  11:28 PM               390 Pipeline-For_GCP_VM.bat
 09-09-2025  11:12 PM    <DIR>          Pipeline-For_Public_GKE_Cluster
-10-09-2025  12:32 AM               390 Pipeline-For_Public_GKE_Cluster.bat
+05-10-2025  11:28 PM               414 Pipeline-For_Public_GKE_Cluster.bat
+05-10-2025  05:22 PM    <DIR>          portfolio
 02-10-2025  01:55 PM    <DIR>          pvt-repo
-10-09-2025  12:32 AM               344 pvt-repo.bat
-               6 File(s)          2,184 bytes
-               8 Dir(s)  118,975,455,232 bytes free
+05-10-2025  11:28 PM               368 pvt-repo.bat
+               6 File(s)          2,328 bytes
+               9 Dir(s)  118,974,894,080 bytes free
