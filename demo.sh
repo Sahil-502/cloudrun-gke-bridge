5251db77e8537335e6d5d7cc684322cb9ee1c3d0
Sahil <sahilhr24@gmail.com>
2025-10-31 00:41:40 +0530

    v1480

--- demo.sh
+++ demo.sh
@@ -15,7 +15,7 @@
 08-10-2025  10:01 PM    <DIR>          Pipeline-For_Public_GKE_Cluster
 05-10-2025  11:28 PM               414 Pipeline-For_Public_GKE_Cluster.bat
 05-10-2025  05:22 PM    <DIR>          portfolio
-05-10-2025  11:29 PM    <DIR>          pvt-repo
+31-10-2025  12:41 AM    <DIR>          pvt-repo
 05-10-2025  11:28 PM               368 pvt-repo.bat
                6 File(s)          2,328 bytes
-               8 Dir(s)  118,974,144,512 bytes free
+               8 Dir(s)  118,974,066,688 bytes free
