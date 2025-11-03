a7fb0a1eb38fa585ed6965b3fd4887f180e88cfa
Sahil <sahilhr24@gmail.com>
2025-11-03 07:54:09 +0530

    v9385

--- demo.sh
+++ demo.sh
@@ -10,7 +10,7 @@
 05-10-2025  11:27 PM               390 cloudrun-gke-bridge.bat
 08-10-2025  10:01 PM    <DIR>          Infra-CLI-Guide
 05-10-2025  11:28 PM               382 Infra-CLI-Guide.bat
-05-10-2025  11:29 PM    <DIR>          Pipeline-For_GCP_VM
+03-11-2025  07:54 AM    <DIR>          Pipeline-For_GCP_VM
 05-10-2025  11:28 PM               390 Pipeline-For_GCP_VM.bat
 08-10-2025  10:01 PM    <DIR>          Pipeline-For_Public_GKE_Cluster
 05-10-2025  11:28 PM               414 Pipeline-For_Public_GKE_Cluster.bat
@@ -18,4 +18,4 @@
 31-10-2025  12:41 AM    <DIR>          pvt-repo
 05-10-2025  11:28 PM               368 pvt-repo.bat
                6 File(s)          2,328 bytes
-               8 Dir(s)  118,974,046,208 bytes free
+               8 Dir(s)  118,974,017,536 bytes free
